6c7bf674b9b03fb03b2093302b85a7107747f496
Viktor Podzigun <viktor.podzigun@gmail.com>
2018-09-07 09:56:25 +0200

    Added scommons-service-test module

--- project/src/main/scala/common/Libs.scala
+++ project/src/main/scala/common/Libs.scala
@@ -6,6 +6,7 @@ object Libs extends CommonLibs {
 
   val scommonsServiceVersion = "0.1.0-SNAPSHOT"
   val scommonsClientVersion = "0.1.0-SNAPSHOT"
+  val scommonsApiVersion = "0.1.0-SNAPSHOT"
 
   //////////////////////////////////////////////////////////////////////////////
   // jvm dependencies
