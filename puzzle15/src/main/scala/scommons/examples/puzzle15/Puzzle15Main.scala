8b20051d844eb875a5de586a0466f952ebc0ba52
Viktor Podzigun <viktor.podzigun@gmail.com>
2019-03-03 13:35:44 +0100

    Updated scala version; switched to sbt 1.x build

--- puzzle15/src/main/scala/scommons/examples/puzzle15/Puzzle15Main.scala
+++ puzzle15/src/main/scala/scommons/examples/puzzle15/Puzzle15Main.scala
@@ -4,11 +4,8 @@ import io.github.shogowada.scalajs.reactjs.ReactDOM
 import io.github.shogowada.scalajs.reactjs.VirtualDOM._
 import org.scalajs.dom
 
-import scala.scalajs.js.annotation.JSExportTopLevel
-
 object Puzzle15Main {
 
-  @JSExportTopLevel("scommons.examples.puzzle15.main")
   def main(args: Array[String]): Unit = {
     val mountNode = dom.document.getElementById("root")
     mountNode.setAttribute("class", Puzzle15ViewCss.view)
